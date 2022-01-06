60417670ec1bac9e6cc3ae92a2ca7ad937f7e31e
AmiReza <Mr.Ara1999@gmail.com>
2022-01-06 13:52:16 +0330

    instagram

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function myfunc() {
+        document.getElementById("caption").style.display = "none";
+        document.getElementById("caption1").style.display = "flex";
+    };
+    function myfunc1() {
+        document.getElementById("caption").style.display = "flex";
+        document.getElementById("caption1").style.display = "none";
+    }
